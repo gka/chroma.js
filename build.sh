dcf0fc0509a1ce0cb589696da672fa1e8c8cde60
Gregor Aisch <contact@vis4.net>
2012-02-02 12:55:48 +0100

    updated build, moved built js into dist/

--- build.sh
+++ build.sh
@@ -7,3 +7,10 @@ coffee -o . -j chroma.colors.src.js src/colors/*.coffee
 cat LICENSE-colors > chroma.colors.js
 uglifyjs chroma.colors.src.js >> chroma.colors.js
 rm chroma.colors.src.js
+
+coffee -o . -j chroma.pack.js src/chroma.coffee src/utils.coffee src/colors/*.coffee
+cat LICENSE-nonsource > chroma.pack.min.js
+cat LICENSE-colors >> chroma.pack.min.js
+uglifyjs chroma.pack.js >> chroma.pack.min.js
+
+mv chroma*.js dist/
