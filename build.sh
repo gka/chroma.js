06d6cd908b8e849bbc10470b28d14d651666eb6b
Gregor Aisch <contact@vis4.net>
2013-03-01 13:18:25 +0100

    improved build script

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-coffee -o . -j chroma.js src/chroma.coffee src/utils.coffee
-cat LICENSE-nonsource > chroma.min.js
-uglifyjs chroma.js >> chroma.min.js
+coffee -o . -j chroma.core.js src/chroma.coffee src/colorscale.coffee src/utils.coffee
+coffee -o . -j chroma.colors.js src/colors/*.coffee
+coffee -o . -j chroma.js src/chroma.coffee src/colorscale.coffee src/utils.coffee src/colors/*.coffee
 
-coffee -o . -j chroma.colors.src.js src/colors/*.coffee
-cat LICENSE-colors > chroma.colors.js
-uglifyjs chroma.colors.src.js >> chroma.colors.js
-rm chroma.colors.src.js
-
-coffee -o . -j chroma.pack.js src/chroma.coffee src/utils.coffee src/colors/*.coffee
-cat LICENSE-nonsource > chroma.pack.min.js
-cat LICENSE-colors >> chroma.pack.min.js
-uglifyjs chroma.pack.js >> chroma.pack.min.js
-
+# cat LICENSE-colors >> chroma.min.js
+uglifyjs chroma.js > chroma.min.js
 mv chroma*.js dist/
