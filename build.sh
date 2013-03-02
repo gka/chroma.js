9ea03c953408317424856b7ff23a88629c2bcd78
Gregor Aisch <contact@vis4.net>
2013-03-02 23:32:04 +0100

    moved packed library to repository root

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 coffee -o . -j chroma.js src/color.coffee src/colorscale.coffee src/limits.coffee src/utils.coffee src/colors/*.coffee
 uglifyjs chroma.js >> chroma.min.js
-mv chroma*.js dist/
+mv chroma*.js
