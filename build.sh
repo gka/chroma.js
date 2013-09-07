a0e126f45287293b4cae8ee63af1f9f0d6e42835
Gregor Aisch <contact@vis4.net>
2013-09-07 13:34:51 +0200

    renamed file colorscale.coffee --> scale.coffee

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-coffee -o . -j chroma.js src/api.coffee src/color.coffee src/conversions/*.coffee  src/colorscale.coffee src/limits.coffee src/colors/*.coffee src/utils.coffee src/interpolate.coffee
+coffee -o . -j chroma.js src/api.coffee src/color.coffee src/conversions/*.coffee  src/scale.coffee src/limits.coffee src/colors/*.coffee src/utils.coffee src/interpolate.coffee
 uglifyjs chroma.js > chroma.min.js
 
