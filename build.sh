7b13191d29fa58f0538c8f94032b32eb859acd15
Gregor Aisch <contact@vis4.net>
2013-11-18 12:24:29 +0100

    including license in build

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
-coffee -o . -j chroma.js src/api.coffee src/color.coffee src/conversions/*.coffee  src/scale.coffee src/limits.coffee src/colors/*.coffee src/utils.coffee src/interpolate.coffee
-uglifyjs chroma.js > chroma.min.js
-
+#!/bin/bash
+echo "###*" > license.coffee
+echo " * @license" >> license.coffee
+echo " *" >> license.coffee
+while read i
+do
+   echo " * $i"  >> license.coffee
+done < LICENSE
+echo "###" >> license.coffee
+coffee -o . -j chroma.js license.coffee src/api.coffee src/color.coffee src/conversions/*.coffee  src/scale.coffee src/limits.coffee src/colors/*.coffee src/utils.coffee src/interpolate.coffee
+uglifyjs --comments "@license" chroma.js > chroma.min.js
+rm license.coffee
