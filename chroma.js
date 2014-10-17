c212a606a17ffaf61d2f3f70a9d7e3eb838bef2c
Thomas Steur <tsteur@users.noreply.github.com>
2014-10-17 16:43:53 +1300

    fix uncaught TypeError: undefined is not a function
    Occurs in latest Chrome and Firefox because of new Ecmascript 6 methods see http://www.sitepoint.com/preparing-ecmascript-6-new-array-methods/
    In this case `data` is an array and has therefore a function "values" meaning it is no longer null

--- chroma.js
+++ chroma.js
@@ -1316,7 +1316,7 @@
     if (num == null) {
       num = 7;
     }
-    if (data.values == null) {
+    if (type(data.values) !== 'array') {
       data = chroma.analyze(data);
     }
     min = data.min;
