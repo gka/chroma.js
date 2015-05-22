c614edcf7ae324a4aa8cdf2ed6d8ce10a8f3d7ef
Gregg Tavares <github@greggman.com>
2015-05-22 22:39:06 +0900

    Build with Grunt
    I don't know if you'll like this or not but why build with Grunt?
    
    1.  It's cross platform (works on Windows)
    
    2.  Less global installs needed
    
        with make I needed make installed (I guess that's common), I also
        needed coffee install, might be the wrong version, and I needed
        uglify installed.
    
        With Grunt those dependencies are installed insided node_modules.
    
        so
    
            git clone git://github.com/gka/chroma.js
            npm install
            grunt
    
        I think you can build without installing grunt as well with
    
            ./node_modules/grunt/bin/grunt
    
    Also I don't really know what you wanted this to do. If you check
    the current chroma.js the license looks like
    
        // Generated by CoffeeScript 1.6.2
        /** echo  * @license echo  * while read i do echo  *  done echo
        */
    
    That's fixed assuming you wanted it fixed
    
    Take it or leave it. I just thought I'd pass it on

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    clean: [
+      'chroma.js',
+      'chroma.min.js',
+      'license.coffee',
+    ],
+    coffee: {
+      compile: {
+        options: {
+          join: true
+        },
+        files: {
+          'chroma.js': [
+            'license.coffee',
+            'src/api.coffee',
+            'src/color.coffee',
+            'src/conversions/*.coffee',
+            'src/scale.coffee',
+            'src/limits.coffee',
+            'src/colors/*.coffee',
+            'src/utils.coffee',
+            'src/interpolate.coffee',
+          ],
+        },
+      }
+    },
+    uglify: {
+      options: {
+        banner: "/*\n" + fs.readFileSync("LICENSE", {encoding: "utf8"}) + "\n*/\n",
+      },
+      my_target: {
+        files: {
+          'chroma.min.js': ['chroma.js']
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('license', function() {
+    var license = [
+      "###*",
+      " * @license",
+      " *",
+    ].concat(fs.readFileSync('LICENSE', {encoding: "utf8"}).split("\n").map(function(line) {
+      return " * " + line;
+    }));
+    license.push("###\n");
+    fs.writeFileSync('license.coffee', license.join("\n"));
+  });
+
+  grunt.registerTask('default', ['clean', 'license', 'coffee', 'uglify']);
+};
+
