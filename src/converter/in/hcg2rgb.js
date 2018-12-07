2c54978adce375f2e908fe3ed72c76723eb461b4
Gregor Aisch <gka@users.noreply.github.com>
2018-12-07 13:15:22 +0100

    hcg

--- src/converter/in/hcg2rgb.js
+++ src/converter/in/hcg2rgb.js
@@ -1,29 +1,41 @@
 const {unpack} = require('../../utils');
+const {floor} = Math;
+
+/*
+ * hue.. [0..360]
+ * chroma .. [0..1]
+ * grayness .. [0..1]
+ */
 
 const hcg2rgb = (...args) => {
-    let [h,c,_g] = unpack(args, 'hcg');
-    c = c / 100;
-    g = g / 100 * 255;
-    _c = c * 255;
-    if c is 0
+    args = unpack(args, 'hcg');
+    let [h,c,_g] = args;
+    let r,g,b;
+    _g = _g * 255;
+    const _c = c * 255;
+    if (c === 0) {
         r = g = b = _g
-    else
-        h = 0 if h is 360
-        h -= 360 if h > 360
-        h += 360 if h < 0
-        h /= 60
-        i = floor h
-        f = h - i
-        p = _g * (1 - c)
-        q = p + _c * (1 - f)
-        t = p + _c * f
-        v = p + _c
-        switch i
-            when 0 then [r,g,b] = [v, t, p]
-            when 1 then [r,g,b] = [q, v, p]
-            when 2 then [r,g,b] = [p, v, t]
-            when 3 then [r,g,b] = [p, q, v]
-            when 4 then [r,g,b] = [t, p, v]
-            when 5 then [r,g,b] = [v, p, q]
-    [r, g, b, if args.length > 3 then args[3] else 1]
+    } else {
+        if (h === 360) h = 0;
+        if (h > 360) h -= 360;
+        if (h < 0) h += 360;
+        h /= 60;
+        const i = floor(h);
+        const f = h - i;
+        const p = _g * (1 - c);
+        const q = p + _c * (1 - f);
+        const t = p + _c * f;
+        const v = p + _c;
+        switch (i) {
+            case 0: [r,g,b] = [v, t, p]; break
+            case 1: [r,g,b] = [q, v, p]; break
+            case 2: [r,g,b] = [p, v, t]; break
+            case 3: [r,g,b] = [p, q, v]; break
+            case 4: [r,g,b] = [t, p, v]; break
+            case 5: [r,g,b] = [v, p, q]; break
+        }
+    }
+    return [r, g, b, args.length > 3 ? args[3] : 1];
 }
+
+module.exports = hcg2rgb;
