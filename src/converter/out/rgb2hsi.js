09b420412ba82e8e03710a060c15c08e68da0054
Gregor Aisch <gka@users.noreply.github.com>
2018-12-08 23:35:33 +0100

    rgb2hsi

--- src/converter/out/rgb2hsi.js
+++ src/converter/out/rgb2hsi.js
@@ -13,9 +13,9 @@ const rgb2hsi = (...args) => {
     let h;
     const min_ = min(r,g,b);
     const i = (r+g+b) / 3;
-    const s = 1 - min_/i;
+    const s = i > 0 ? 1 - min_/i : 0;
     if (s === 0) {
-        h = 0
+        h = NaN;
     } else {
         h = ((r-g)+(r-b)) / 2;
         h /= sqrt((r-g)*(r-g) + (r-b)*(g-b));
--- test/unit/rgb2hsi.test.js
+++ test/unit/rgb2hsi.test.js
@@ -5,17 +5,23 @@ require('es6-shim');
 const rgb2hsi = require('../../src/converter/out/rgb2hsi');
 
 const tests = {
-    black:      { in: [0,0,0],     out: [0,0,0]},
-    white:      { in: [0,0,1],     out: [255,255,255]},
-    gray:       { in: [0,0,0.5],   out: [127.5,127.5,127.5]},
-    red:        { in: [0,1,0.5],   out: [255,0,0]},
-    yellow:     { in: [60,1,0.5],  out: [255,255,0]},
-    green:      { in: [120,1,0.5], out: [0,255,0]},
-    cyan:       { in: [180,1,0.5], out: [0,255,255]},
-    blue:       { in: [240,1,0.5], out: [0,0,255]},
-    magenta:    { in: [300,1,0.5], out: [255,0,255]},
-    red_again:  { in: [360,1,0.5], out: [255,0,0]}
+    black2:     { hsi: [NaN,0,0],   rgb: [0,0,0,1]},
+    white:      { hsi: [NaN,0,1],   rgb: [255,255,255,1]},
+    gray:       { hsi: [NaN,0,0.5],   rgb: [127.5,127.5,127.5,1]},
+    red:        { hsi: [0,1,1/3],   rgb: [255,0,0,1]},
+    yellow:     { hsi: [60,1,2/3],  rgb: [255,255,0,1]},
+    green:      { hsi: [120,1,1/3], rgb: [0,255,0,1]},
+    cyan:       { hsi: [180,1,2/3], rgb: [0,255,255,1]},
+    blue:       { hsi: [240,1,1/3], rgb: [0,0,255,1]},
+    magenta:    { hsi: [300,1,2/3], rgb: [255,0,255,1]},
 };
+
+const round = (digits) => {
+    const d = Math.pow(10,digits);
+    return (v) => Math.round(v*d) / d;
+}
+
+const rnd = round(5);
 
 const batch = {};
 
@@ -23,15 +29,15 @@ Object.keys(tests).forEach(key => {
     batch[`rgb2hsi ${key}`] = {
         topic: tests[key],
         array(topic) {
-            assert.deepStrictEqual(rgb2hsi(topic.out), topic.in);
+            assert.deepStrictEqual(rgb2hsi(topic.rgb).map(rnd), topic.hsi.map(rnd));
         },
         obj(topic) {
-            let [r,g,b] = topic.out;
-            assert.deepStrictEqual(rgb2hsi({r,g,b}), topic.in);
+            let [r,g,b] = topic.rgb;
+            assert.deepStrictEqual(rgb2hsi({r,g,b}).map(rnd), topic.hsi.map(rnd));
         },
         args(topic) {
             if (topic.mode != 'auto') return
-            assert.deepStrictEqual(rgb2hsi.apply(null, topic.out), topic.in);
+            assert.deepStrictEqual(rgb2hsi.apply(null, topic.rgb).map(rnd), topic.hsi.map(rnd));
         }
     }
 });
