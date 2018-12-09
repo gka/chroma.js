6f60267cdf67b02f395547857f02c0c5940e2409
Gregor Aisch <gka@users.noreply.github.com>
2018-12-09 01:27:53 +0100

    added rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
+
+const production = !process.env.ROLLUP_WATCH && !process.env.ENV == 'dev';
+
+module.exports = {
+    input: 'index.js',
+    output: {
+        file: 'chroma.js',
+        format: 'cjs'
+    },
+    name: 'chroma',
+    plugins: [
+        resolve(),
+        commonjs(),
+
+        // If we're building for production (npm run build
+        // instead of npm run dev), transpile and minify
+        production && buble({
+            // transforms: { dangerousForOf: true }
+        }),
+        production && uglify({
+            mangle: true
+        })
+
+    ]
+};
