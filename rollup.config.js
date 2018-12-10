bf158235e8bef93ddc60a27a7971709fb7c1277e
Gregor Aisch <gka@users.noreply.github.com>
2018-12-10 23:38:09 +0100

    build step

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
+import license from 'rollup-plugin-license';
+import path from 'path';
+
 import {uglify} from 'rollup-plugin-uglify';
 
-const production = !process.env.ROLLUP_WATCH;// && !process.env.ENV == 'dev';
+/** globals process, __dirname **/
+
+const production = !process.env.ROLLUP_WATCH && !process.env.DEV;
 
 module.exports = {
     input: 'index.js',
     output: {
-        file: 'chroma.js',
-        format: 'cjs',
+        file: `chroma${production?'.min':''}.js`,
+        format: 'umd',
         name: 'chroma',
     },
     plugins: [
@@ -18,12 +23,20 @@ module.exports = {
 
         // If we're building for production (npm run build
         // instead of npm run dev), transpile and minify
-        production && buble({
+        buble({
             transforms: { dangerousForOf: true }
         }),
         production && uglify({
             mangle: true
-        })
+        }),
+        license({
+            sourceMap: true,
+            cwd: '.', // Default is process.cwd()
 
+            banner: {
+                file: path.join(__dirname, 'LICENSE'),
+                encoding: 'utf-8', // Default is utf-8
+            }
+        }),
     ]
 };
