b981678cf7ef1600f0853f750022b75316cd6bb5
Gregor Aisch <gka@users.noreply.github.com>
2018-12-10 21:27:38 +0100

    fixed clip_rgb bug

--- src/ops/clipped.js
+++ src/ops/clipped.js
@@ -1,5 +1,5 @@
 const Color = require('../Color');
 
 Color.prototype.clipped = function() {
-    return this._rgb.clipped || false;
+    return this._rgb._clipped || false;
 }
--- src/utils/clip_rgb.js
+++ src/utils/clip_rgb.js
@@ -5,7 +5,7 @@ module.exports = (rgb) => {
     rgb._unclipped = rgb.slice(0);
     for (let i=0; i<=3; i++) {
         if (i < 3) {
-            rgb._clipped = rgb[i] < 0 || rgb[i] > 255;
+            if (rgb[i] < 0 || rgb[i] > 255) rgb._clipped = true;
             rgb[i] = limit(rgb[i], 0, 255);
         } else if (i === 3) {
             rgb[i] = limit(rgb[i], 0, 1);
