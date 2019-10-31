6d14b9c8d43db67b186d56e9c286a3a7dc144013
Hofer ivan <ivan.hofer@outlook.com>
2019-10-31 08:40:07 +0100

    make chroma.js work on IE11 (#232)
    * maske build-script work on windows
    * change includes to indexOf (resolves #225)

--- src/io/hex/index.js
+++ src/io/hex/index.js
@@ -15,7 +15,7 @@ input.format.hex = require('./hex2rgb');
 input.autodetect.push({
     p: 4,
     test: (h, ...rest) => {
-        if (!rest.length && type(h) === 'string' && [3,4,6,7,8,9].includes(h.length)) {
+        if (!rest.length && type(h) === 'string' && [3,4,6,7,8,9].indexOf(h.length) >= 0) {
             return 'hex';
         }
     }
